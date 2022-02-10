0481eaeaff34b56fe601655c183b709ec4b2a4cd
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-02-10 18:07:54 +0900

    Make a WeTube server, routers and controllers

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+
+import globalRouter from "./routers/globalRouter"
+import userRouter from "./routers/userrouter"
+import videoRouter from "./routers/videoRouter"
+
+
+const PORT = 5678;
+const app = express();
+const logger = morgan("dev");
+
+app.use(logger);
+
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+
+
+app.listen(PORT, handleListening);
