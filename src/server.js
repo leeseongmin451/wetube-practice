4a79d41c7a32a2b16292f62f9577a9934629351f
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-02-19 23:24:13 +0900

    Create pug files to simplify html

--- src/server.js
+++ src/server.js
@@ -6,13 +6,13 @@ import userRouter from "./routers/userrouter"
 import videoRouter from "./routers/videoRouter"
 
 
-const PORT = 5678;
+const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
-
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
