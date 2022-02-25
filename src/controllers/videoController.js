5e03c32e7294745273a9023070d53455e794f780
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-02-25 23:53:51 +0900

    6.26 Search part One
     - Search 페이지를 새로 만들고, 검색창과 버튼을 이용하여 검색 키워드를 생성하는 기능을 추가했습니다.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -61,4 +61,12 @@ export const deleteVideo = async (req, res) => {
     const {id} = req.params;
     await Video.findByIdAndDelete(id);
     return res.redirect("/");
+};
+
+export const search = (req, res) => {
+    const {keyword} = req.query;
+    if (keyword) {
+        // search
+    }
+    return res.render("search", {pageTitle: "Search"});
 }
