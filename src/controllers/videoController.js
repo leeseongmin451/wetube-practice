ac686b991c7c31798cad66af226af390d5114434
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-02-25 23:22:07 +0900

    6.24 Statics
     - Video 모델에 대한 새로운 함수를 static을 사용하여 직접 정의하고 적용하여 hashtag를 다루는 코드를 더 간결화하였습니다.
     - Hashtag 또한 홈페이지의 동영상 목록에 출력되도록 `home.pug` 파일을 수정하였습니다.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -34,7 +34,7 @@ export const postEdit = async (req, res) => {
     await Video.findByIdAndUpdate(id, {
         title, 
         description, 
-        hashtags: hashtags, 
+        hashtags: Video.formatHashtags(hashtags)
     });
     return res.redirect(`/videos/${id}`);
 };
@@ -49,7 +49,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title, 
             description, 
-            hashtags: hashtags, 
+            hashtags: Video.formatHashtags(hashtags), 
         });
         return res.redirect("/");
     } catch(error) {
