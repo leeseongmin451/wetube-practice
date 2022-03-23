1cf600827e726cf4bd8515296cec8c3d020aaefb
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-03-23 23:00:47 +0900

    7.9 Logged In User
    유저의 로그인 상태와 해당 유저 정보를 세션에 저장하도록 하였습니다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -63,7 +63,8 @@ export const postLogin = async (req, res) => {
             errorMessage: "Wrong password", 
         });
     }
-
+    req.session.loggedIn = true;
+    req.session.user = user;
     return res.redirect("/");
 };
 
