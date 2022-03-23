21052150061af5f97af5f93cf43aaf361b2dc627
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-03-23 22:35:49 +0900

    7.7 Sessions and Cookies part One
    세션 처리를 위해 `express-session`을 설치하고 사용합니다.

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import app from "./server";
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+const handleListening = () => console.log(`Server listening on http://localhost:${PORT}`);
 
 
 app.listen(PORT, handleListening);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 
 import globalRouter from "./routers/rootRouter"
 import userRouter from "./routers/userrouter"
@@ -13,6 +14,13 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "Hello!", 
+    resave: true, 
+    saveUninitialized: true, 
+}));
+
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
