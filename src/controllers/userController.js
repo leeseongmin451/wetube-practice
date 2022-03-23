451314bee44da1badf5df72bcfd78d902a320d2e
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-03-23 22:03:14 +0900

    7.5 Login part One
    로그인 페이지를 만들고, 사용자가 존재하지 않을 경우에 대한 예외처리를 정의하였습니다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,21 +20,42 @@ export const postJoin = async (req, res) => {
             errorMessage: "This username/email is already taken.", 
         });
     };
-    await User.create({
-        name, 
-        username, 
-        email, 
-        password, 
-        location, 
-    });
-    return res.redirect("/login");
+    try {
+        await User.create({
+            name, 
+            username, 
+            email, 
+            password, 
+            location, 
+        });
+        return res.redirect("/login");
+    } catch(error) {
+        return res.status(400).render("join", {
+            pageTitle: "Join", 
+            errorMessage: error._message, 
+        });
+    };
 };
 
 export const edit = (req, res) => res.send("Edit User");
 
 export const remove = (req, res) => res.send("Delete User");
 
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) => res.render("login", {pageTitle: "Log In"});
+
+export const postLogin = async (req, res) => {
+    const {username, password} = req.body;
+    const exists = await User.exists({username});
+
+    if (!exists) {
+        return res.status(400).render("login", {
+            pageTitle: "Log In", 
+            errorMessage: "An account with this username does not exists."
+        });
+    }
+
+    res.end();
+};
 
 export const logout = (req, res) => res.send("Log Out");
 
