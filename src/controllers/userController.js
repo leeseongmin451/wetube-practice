5e695ee2fac10a769c852e8885835f4f0c2afe14
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-03-26 21:46:25 +0900

    7.17 Github Login part Two
    Github의 사용자 정보 요청 시 설정값을 지정하는 URL을 만들어주는 router를 만들었습니다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -68,6 +68,22 @@ export const postLogin = async (req, res) => {
     return res.redirect("/");
 };
 
+export const startGithubLogin = (req, res) => {
+    const baseUrl = `https:// github.com/login/oauth/authorize?`;
+    const config = {
+        client_id: "3536776373406f708c2f", 
+        allow_signup: false, 
+        scope: "read:user user:email", 
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {
+    
+}
+
 export const logout = (req, res) => res.send("Log Out");
 
 export const see = (req, res) => res.send("See User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import { edit, remove, logout, see, startGithubLogin, finishGithubLogin } from "../controllers/userController";
 
 
 const userRouter = express.Router();
@@ -7,6 +7,8 @@ const userRouter = express.Router();
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/start", finishGithubLogin);
 userRouter.get(":id", see);
 
 
