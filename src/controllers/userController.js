d4bcb1db2f2791f2a7107bb9de33eb4dd7110c0a
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-03-27 00:29:48 +0900

    7.22 Log Out
    로그아웃 기능을 구현하였습니다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -41,14 +41,12 @@ export const postJoin = async (req, res) => {
 
 export const edit = (req, res) => res.send("Edit User");
 
-export const remove = (req, res) => res.send("Delete User");
-
 export const getLogin = (req, res) => res.render("login", {pageTitle: "Log In"});
 
 export const postLogin = async (req, res) => {
     const {username, password} = req.body;
     const pageTitle = "Log In";
-    const user = await User.findOne({username});
+    const user = await User.findOne({username, socialOnly: false});
 
     if (!user) {
         return res.status(400).render("login", {
@@ -121,13 +119,10 @@ export const finishGithubLogin = async (req, res) => {
         if (!emailObj) {
             return res.redirect("/login");
         }
-        const existingUser = await User.findOne({email: emailObj.email});
-        if (existingUser) {
-            req.session.loggedIn = true;
-            req.session.user = existingUser;
-            return res.redirect("/");
-        } else {
-            const user = await User.create({
+        let user = await User.findOne({email: emailObj.email});
+        if (!user) {
+            user = await User.create({
+                avatarUrl: userData.avatar_url, 
                 name: userData.name, 
                 username: userData.login, 
                 email: emailObj.email, 
@@ -135,15 +130,18 @@ export const finishGithubLogin = async (req, res) => {
                 socialOnly: true, 
                 location: userData.location, 
             });
-            req.session.loggedIn = true;
-            req.session.user = user;
-            return res.redirect("/");
         }
+        req.session.loggedIn = true;
+        req.session.user = user;
+        return res.redirect("/");
     } else {
         return res.redirect("/login");
     }
 }
 
-export const logout = (req, res) => res.send("Log Out");
+export const logout = (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+};
 
 export const see = (req, res) => res.send("See User");
