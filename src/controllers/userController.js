52e58151122982a3a0ce7d194713c52ef88e2d4e
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-03-28 17:54:48 +0900

    8.0 Edit Profile GET
    로그인 중인 유저의 프로필을 수정할 수 있는 페이지와 라우터, 컨트롤러를 생성하였습니다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -39,8 +39,6 @@ export const postJoin = async (req, res) => {
     };
 };
 
-export const edit = (req, res) => res.send("Edit User");
-
 export const getLogin = (req, res) => res.render("login", {pageTitle: "Log In"});
 
 export const postLogin = async (req, res) => {
@@ -142,6 +140,14 @@ export const finishGithubLogin = async (req, res) => {
 export const logout = (req, res) => {
     req.session.destroy();
     return res.redirect("/");
+};
+
+export const getEdit = (req, res) => {
+    return res.render("edit-profile", {pageTitle: "Edit Profile"});
+};
+
+export const postEdit = (req, res) => {
+    return res.render("edit-profile");
 };
 
 export const see = (req, res) => res.send("See User");
