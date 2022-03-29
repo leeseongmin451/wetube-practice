f44730babd2e9e792afa5e2329ed87329c0de819
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-03-29 14:37:35 +0900

    8.4 Change Password part One
    사용자 비밀번호 변경 페이지와 URL을 만들고 라우터에 반영하였습니다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -163,4 +163,14 @@ export const postEdit = async (req, res) => {
     return res.render("edit-profile");
 };
 
+export const getChangePassword = (req, res) => {
+    if (req.session.user.socialOnly) {
+        return res.redirect("/");
+    }
+    return res.render("change-password", {pageTitle: "Change Password"});
+};
+export const postChangePassword = (req, res) => {
+    return res.redirect("/");
+};
+
 export const see = (req, res) => res.send("See User");
