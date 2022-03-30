a1763011b3e74b968557a02cccdfdfa9826c5eb0
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-03-30 13:06:24 +0900

    8.9 Video Upload
    Video 객체에 실제 비디오 파일을 업로드하고, 재생 가능하도록 허였습니다.

--- src/middlewares.js
+++ src/middlewares.js
@@ -23,6 +23,9 @@ export const publicOnlyMiddleware = (req, res, next) => {
     }
 }
 
-export const uploadFiles = multer({
-    dest: "uploads/"
-});
+export const avatarUpload = multer({dest: "uploads/avatars/", limits: {
+    fileSize: 8000000, 
+}});
+export const videoUpload = multer({dest: "uploads/videos/", limits: {
+    fileSize: 1000000000, 
+}});
