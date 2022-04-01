dd2c68eba089c2154e453bb2e7a171bc7d85304f
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-04-01 13:32:47 +0900

    11.3 Volume
    볼륨 조절 바의 기능을 구현했습니다.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,9 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 const handlePlayClick = (e) => {
     if (video.paused) {
         video.play();
@@ -13,23 +16,34 @@ const handlePlayClick = (e) => {
     playBtn.innerText = video.paused ? "Play" : "Pause";
 }
 
-const handlePause = (e) => {
+const handlePause = (event) => {
     playBtn.innerText = "Play"
 }
 
-const handlePlay = (e) => {
+const handlePlay = (event) => {
     playBtn.innerText = "Pause"
 }
 
-const handleMute = (e) => {
+const handleMute = (event) => {
     if (video.muted) {
         video.muted = false;
     } else {
         video.muted = true;
     }
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-    volumeRange.value = video.muted ? 0 : 0.5;
+    volumeRange.value = video.muted ? 0 : volumeValue;
+}
+
+const handleVolumeChange = (event) => {
+    const {target: {value}} = event;
+    if (video.muted) {
+        video.muted = false
+        muteBtn.innerText = "Mute"
+    }
+    volumeValue = value
+    video.volume = value;
 }
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
