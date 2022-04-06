0c49b9679c5e93a6ac3355b835f43e86ca5a96fd
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-04-06 11:00:34 +0900

    13.3 Recording Video part Two
    녹화한 동영상을 루프 형태로 반복 재생하여 보여줄 수 있도록 하고, 녹화 버튼을 다운로드 버튼으로 변경하도록 하였습니다.

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,11 +2,18 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
+let recorder;
+
+const handleDownload = () => {
+
+}
 
 const handleStop = () => {
-    startBtn.innerText = "Start Recording"
+    startBtn.innerText = "Download Recording"
     startBtn.removeEventListener("click", handleStop);
-    startBtn.addEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleDownload);
+
+    recorder.stop();
 }
 
 const handleStart = () => {
@@ -14,19 +21,20 @@ const handleStart = () => {
     startBtn.removeEventListener("click", handleStart);
     startBtn.addEventListener("click", handleStop);
 
-    const recorder = new MediaRecorder(stream);
-    recorder.ondataavailable = (e) => {
-        
+    recorder = new window.MediaRecorder(stream);
+    recorder.ondataavailable = (event) => {
+        const videoFile = URL.createObjectURL(event.data);
+        video.srcObject = null;
+        video.src = videoFile;
+        video.loop = true;
+        video.play();
     };
     recorder.start();
-    setTimeout(() => {
-        recorder.stop();
-    }, 10000);
 };
 
 const init = async () => {
     stream = await navigator.mediaDevices.getUserMedia({
-        audio: true, 
+        audio: false, 
         video: true, 
     });
     video.srcObject = stream;
