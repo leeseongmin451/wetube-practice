c95cc4a9faf8ab1b442336aaaa06ff86808d0be3
leeseongmin451 <80591422+leeseongmin451@users.noreply.github.com>
2022-04-11 15:11:55 +0900

    API Route part One
    API 라우팅 url을 생성하여 작성한 댓글 내용과 해당 댓글이 달릴 동영상 ID를 백엔드로 전송하도록 허였습니다.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -112,3 +112,7 @@ export const registerView = async (req, res) => {
     await video.save();
     return res.sendStatus(200);
 }
+
+export const createComment = (req, res) => {
+    return res.end();
+}
